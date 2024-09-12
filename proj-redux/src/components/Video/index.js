3d2de836f3e8299522504816c09c04c619a2f1b2
Daiane Cicuto <ra-21047181-5@alunos.unicesumar.edu.br>
2024-09-12 01:52:09 -0300

    Adicionando arquivos

---
+++ proj-redux/src/components/Video/index.js
@@ -0,0 +1,13 @@
+//Aqui onde vão estar as rotas de video
+import React from 'react';
+
+//abaixo importamos o BrowserRouter e Route
+export default function Video() {
+    return (
+        <div>
+            <strong>Módulo X</strong>
+            <span>Aula X</span> 
+            <h1>Video</h1>
+        </div>
+    );
+}
