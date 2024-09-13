d2ac537c14097c83c466375e5afabd38bc23c813
Daiane Cicuto <ra-21047181-5@alunos.unicesumar.edu.br>
2024-09-13 01:44:21 -0300

    Tentando arrumar a bagunça que eu fiz

--- proj-redux/src/App.js
+++ proj-redux/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'; // Importar o Provider do react-redux
 import Video from './components/Video';
 import Sidebar from './components/Sidebar';
-import store from './store';
+import store from './store'; // Importar a store
 
 class App extends Component {
-  
-
   render() {
     return (
       <div className="App">
-        <Provider store={store}>
-        <Video />
-        <Sidebar/>
+        <Provider store={store}> {/* Usar o Provider para fornecer a store */}
+          <Video />
+          <Sidebar />
+        </Provider>
       </div>
     );
   }
